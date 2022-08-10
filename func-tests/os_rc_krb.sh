79901b5039d314bd0f0f55ca5e241586cfd7e8a7
Vitor Santa Rosa <vitor.rosa@nubank.com.br>
2022-08-10 17:33:10 +0200

    Refactor project

---
+++ func-tests/os_rc_krb.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export OS_AUTH_URL="https://keystone.cern.ch/v3"
+export OS_REGION_NAME="cern"
+export OS_PROJECT_NAME="IT Cloud Infrastructure Developers"
+export OS_PROJECT_DOMAIN_ID="default"
+
+export OS_USERNAME="vsantaro"
+
+export OS_PROTOCOL="kerberos"
+export OS_MUTUAL_AUTH="disabled"
+export OS_INTERFACE="public"
+export OS_AUTH_TYPE="v3fedkerb"
+export OS_IDENTITY_PROVIDER="sssd"
+export OS_VOLUME_API_VERSION="2"
+export OS_IDENTITY_API_VERSION="3"
---
+++ func-tests/os_rc_token.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export OS_AUTH_URL="https://keystone.cern.ch/v3"
+export OS_REGION_NAME="cern"
+export OS_PROJECT_NAME="IT Cloud Infrastructure Developers"
+export OS_PROJECT_DOMAIN_ID="default"
+
+export OS_USERNAME="vsantaro"
+
+# export OS_TOKEN=""
+export OS_AUTH_TYPE="token"
+export OS_VOLUME_API_VERSION="2"
+export OS_IDENTITY_API_VERSION="3"
